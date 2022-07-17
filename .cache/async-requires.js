4758e5d49e09ea9c3cd37d4e2e2c2d9840918557
ottoqwq <ottodeng9@gmail.com>
2022-07-17 09:54:36 +0800

    modified cache files

--- .cache/async-requires.js
+++
@@ -1,5 +0,0 @@
-exports.components = {
-  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
-}
-
